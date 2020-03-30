4425152e4ecc4594ace6e912bef684bbe0ed0857
rocky <rickie.lea.owens@gmail.com>
2020-03-29 20:56:29 -0400

    4e essai

--- main.cpp
+++ main.cpp
@@ -47,9 +47,9 @@ int menuUno(){
     cin >> num1;
     cout << "Entrez le dénominateur: ";
     cin >> num2;
-    
+
     int reponse = calculerPGCD(num1, num2);
-    
+
     std::string solutionPGCD = ("Le Plus Grand Diviseur Commun de " + //print out pgcd
     std::to_string(num1) + " et " +
     std::to_string(num2) + " est " +
@@ -65,27 +65,27 @@ int menuUno(){
     int menuDuo(){
         int num1;
         int num2;
-     
+
        cout << BOLDGREEN << "\t\t  ***          Simplifier une fraction          ***\t\t  " << endl;
        cout << "Entrez le numérateur: "; //get user input
        cin >> num1;
        cout << "Entrez le dénominateur: ";
        cin >> num2;
-       
+
        int pgdc = calculerPGCD(num1, num2);
-        
+
         int simp1 = num1 / pgdc;
         int simp2 = num2 / pgdc;
-        
+
        std::string solutionSimp = std::to_string(simp1) + " \\ " + //print out pgcd
        std::to_string(simp2)+ "\n";
        return 0;
     }//menuDuo
-    
+
 
 // Case 3 : simplifierPlus
 void simplifierPlus(){
-   
+
 } //simplifierPlus
 
 // Case 4: Quitter
@@ -104,7 +104,7 @@ void quitter(){
 int choix = 0;
 
 int main() {
-    
+
     cout << BOLDMAGENTA << "\t\t ****PGCD et simplification des fractions****\b\t\t " << endl;
     cout << "\t\t  ***                MENU           ***\t\t  " << endl;
     cout << std::left;
@@ -113,9 +113,9 @@ int main() {
     << std::setw(6) << "2. Simplifier une fraction?\n" << std::right
     << std::setw(6) << "3. Simplifier plusieurs fractions?\n" << std::right
     << std::setw(6) << "4. Quitter?\n" << std::right << endl;
-    
+
     cin>>choix;
-    
+
     switch(choix) {
         case 1: {
             int solutionPGCD = menuUno();
@@ -137,8 +137,7 @@ int main() {
         }
         default :
         cout << "Choix non-valide. Essaie de nouveau : " << endl;
-        
+
     } // switch
 } //main
 
-                  
