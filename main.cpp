6a8b36ea9eb5f1f294cfc8b9deb124ffe9794da7
rocky <rickie.lea.owens@gmail.com>
2020-03-29 20:34:47 -0400

    3e ajout

--- main.cpp
+++ main.cpp
@@ -35,41 +35,62 @@ using std::endl;
 
 /*Functions*/
 //Case 1 : PGCD
+int calculerPGCD(int a, int b) {
+    return b == 0 ? a : calculerPGCD(b, a % b);
+} //calculerPGCD
+
 int menuUno(){
     int num1;
     int num2;
-    
-    cout << BOLDYELLOW << "Entrez le numérateur: "; //get user input
+    cout << BOLDYELLOW << "\t\t  ***          Calculer le PGDC          ***\t\t  " << endl;
+    cout << "Entrez le numérateur: "; //get user input
     cin >> num1;
     cout << "Entrez le dénominateur: ";
     cin >> num2;
     
-    int reponse = calculerPGCD(<#int a#>, <#int b#>);
+    int reponse = calculerPGCD(num1, num2);
     
-    std::string solutionPGCD = "Le Plus Grand Diviseur Commun de " + //print out pgcd
+    std::string solutionPGCD = ("Le Plus Grand Diviseur Commun de " + //print out pgcd
     std::to_string(num1) + " et " +
     std::to_string(num2) + " est " +
-    std::to_string(reponse);
-    
-}
-
-int calculerPGCD(int a, int b) {
-    return b == 0 ? a : calculerPGCD(b, a % b);
-} //PGCD
+    std::to_string(reponse) + "\n");
+    return reponse;
+}//menuUno
 
 //Case 2: Simplifier
-void simplifier(){
+
+// simplifier
+
+
+    int menuDuo(){
+        int num1;
+        int num2;
+     
+       cout << BOLDGREEN << "\t\t  ***          Simplifier une fraction          ***\t\t  " << endl;
+       cout << "Entrez le numérateur: "; //get user input
+       cin >> num1;
+       cout << "Entrez le dénominateur: ";
+       cin >> num2;
+       
+       int pgdc = calculerPGCD(num1, num2);
+        
+        int simp1 = num1 / pgdc;
+        int simp2 = num2 / pgdc;
+        
+       std::string solutionSimp = std::to_string(simp1) + " \\ " + //print out pgcd
+       std::to_string(simp2)+ "\n";
+       return 0;
+    }//menuDuo
     
-} // simplifier
 
 // Case 3 : simplifierPlus
 void simplifierPlus(){
-    
+   
 } //simplifierPlus
 
 // Case 4: Quitter
 void quitter(){
-    cout << "\t\t\t  ***                Au revoir!           ***\t\t\t  " << endl;
+    cout << BOLDMAGENTA << "\t\t  ***                Au revoir!           ***\t\t  " << endl;
     exit(0);
 }
 
@@ -84,36 +105,38 @@ int choix = 0;
 
 int main() {
     
-    cout << BOLDMAGENTA << "\t\t\t ****PGCD et simplification des fractions****\t\t\t " << endl;
-    cout << "\t\t\t  ***                MENU           ***\t\t\t  " << endl;
+    cout << BOLDMAGENTA << "\t\t ****PGCD et simplification des fractions****\b\t\t " << endl;
+    cout << "\t\t  ***                MENU           ***\t\t  " << endl;
     cout << std::left;
-    cout << RESET << "Voulez-vous:\n" << std::left
+    cout << "Voulez-vous:\n" << std::left
     << std::setw(6) << "1. Trouver le plus grand diviseur commun de deux numéros?\n" << std::right
     << std::setw(6) << "2. Simplifier une fraction?\n" << std::right
     << std::setw(6) << "3. Simplifier plusieurs fractions?\n" << std::right
     << std::setw(6) << "4. Quitter?\n" << std::right << endl;
     
-    
-    cin >> choix;
-    while (choix < 1 || choix > 4){
-        cout << "Choix incorrect.\n Essaye de nouveau : " << endl;
-    }
+    cin>>choix;
     
     switch(choix) {
-        case 1:
-            int solutionPGCD = 0;
-            solutionPGCD = menuUno();
+        case 1: {
+            int solutionPGCD = menuUno();
             std::cout << solutionPGCD;
             break;
-        case 2:
-            simplifier();
+        }
+        case 2: {
+            int solutionSimp = menuDuo();
+            std::cout << solutionSimp;
             break;
-        case 3:
+        }
+        case 3: {
             simplifierPlus();
             break;
-        case 4:
+        }
+        case 4: {
             quitter();
             break;
+        }
+        default :
+        cout << "Choix non-valide. Essaie de nouveau : " << endl;
         
     } // switch
 } //main
